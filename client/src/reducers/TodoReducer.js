3a7a1916340b862a1e01c0ba81e9cf48be62557e
igor <embiggenerd@gmail.com>
2020-01-19 18:24:08 -0500

    added reducer folder and file

---
+++ client/src/reducers/TodoReducer.js
@@ -0,0 +1,12 @@
+export const initialStateTodo = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const reducerTodo = (state, action) => {
+    switch (action.type) {
+        default:
+         return state
+    }
+}
